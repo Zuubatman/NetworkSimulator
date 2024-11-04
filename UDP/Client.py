7b4acb97d130a7a31dc5f08eb304531e9e8dac34
João Vitor Morandi <46168210+Zuubatman@users.noreply.github.com>
2024-11-04 19:57:08 -0300

    Add files via upload

--- UDP/Client.py
+++ UDP/Client.py
@@ -1,15 +1,53 @@
 from socket import *
+import threading
 serverName = 'localhost'
 
 serverPort = 40000
 
 clientSocket = socket(AF_INET, SOCK_DGRAM)
+clientSocket.settimeout(2)
 
-message = input('Input lowercase sentence:')
+#Thread para ouvir as respostas do servidor       
+def listen():
+    while(True):
+        try:
+            response, _  = clientSocket.recvfrom(2048)
+            print (response.decode())
+            
+        except timeout:  
+            continue  
 
-clientSocket.sendto(message.encode(),(serverName, serverPort))
-modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+        except  ():
+            if not threading.main_thread().is_alive(): break
 
-print (modifiedMessage.decode())
+#Thread para enviar respostas ao servidor        
+def sendToServer():    
+    print("Insert a Command:")
+    while(True):
+        command = input("")
+        
+        if('pmf' in command):
+            dataArr = command.split(' ')
+            userDestination = dataArr[1]
+            
+            filePath = input("Insert file path:")
+            with open(filePath, 'rb') as file:
+                content = file.read()
+               
+            message = 'file '+ userDestination+ ' <file> \n'+ content.decode() 
+            clientSocket.sendto(message.encode(),(serverName, serverPort))
+            
+        elif(command == "end"):
+            clientSocket.close()
+    
+        else:
+            clientSocket.sendto(command.encode(),(serverName, serverPort))
 
-clientSocket.close()
+
+#Icialização das Threads, uma para ouvir o servidor e outra pra enviar respostas
+def run():
+    threading.Thread(target=listen).start()
+    threading.Thread(target=sendToServer).start()
+
+run()    
+    
--- UDP/Server.py
+++ UDP/Server.py
@@ -1,17 +1,134 @@
 from socket import *
+import threading
+
 serverPort = 40000
+
 serverSocket = socket(AF_INET, SOCK_DGRAM)
+
 serverSocket.bind(('', serverPort))
 
-print ("The server is ready to receive")
-       
-while True:
-    message, clientAddress = serverSocket.recvfrom(2048)
+serverSocket.settimeout(2)
 
-    decodedMEssage = message.decode()
+clients = []
 
-    if(decodedMEssage == "fim"):
-            serverSocket.sendto('Encerrando conexão...'.encode(), clientAddress)
-            break
+#Função que trata o registro de usuários  
+def registerUser(username, clientAddress):
+    for c in clients: 
+        if c['username'] == username:  
+            return False 
+            
+    clients.append({'username': username, 'clientAddress': clientAddress})
+    return True
+
+#Função que trata o envio de broadcasts       
+def broadCast(message, client_address):
+    senderUsername = None 
+    for c1 in clients:
+        if c1['clientAddress'] == client_address:
+            senderUsername = c1['username']
+    
+    for c in clients:  
+        if c['clientAddress'] != client_address:
+            message2 = "Broadcast from <" + senderUsername + ">: " + message
+            serverSocket.sendto(message2.encode(), c['clientAddress'])
+            
+#Função que trata o envio de mensagens privadas             
+def privateMessage(message, user_destination, client_address):
+    senderUsername = None 
+    for c1 in clients:
+        if c1['clientAddress'] == client_address:
+            senderUsername = c1['username']
+    
+    for c in clients:  
+        if c['username'] == user_destination:
+            message = "<" + senderUsername + ">: " + message
+            serverSocket.sendto(message.encode(), c['clientAddress'])
+
+#Função que trata o envio de arquivos             
+def sendFilePrivate(client_address, userDestination, fileContent):
+    senderUsername = None 
+    for c1 in clients:
+        if c1['clientAddress'] == client_address:
+            senderUsername = c1['username']
+            
+    for c in clients:
+        if c['username'] == userDestination:
+            message = "<" + senderUsername + "> Enviou um arquivo: \n" + fileContent + " \n<file>"
+            serverSocket.sendto(message.encode(), c['clientAddress'])
+
+# def removeFromClients(clientAddress):
+#     clientAux = []
+    
+#     for c in clients:
+#         if c['clientAddress'] != clientAddress:
+#             clientAux.append(c)
+            
+#     clients = clientAux
+
+    
+#Função para tratar os comandos dos clientes                           
+def client(command, clientAddress):
+    command = command.decode().rstrip()
+    
+    if(command.strip() == 'end'):
+        print(F"Closing connection with {clientAddress}")
+        # removeFromClients(clientAddress)
+        
+    elif(command.split(' ')[0] == 'help'):
+        serverSocket.sendto('Commands available: \nREG [username] - Register user\nALL [message] - Broadcast message\nPM [destination] [message] - Private message\nPMF [destination] - Private message with file'.encode(), clientAddress)        
+    
+    elif(command.split(' ')[0] == 'reg'):
+        dataArr = command.split(' ')
+        data = dataArr[1]
+        register_status = registerUser(data, clientAddress)
+            
+        if(register_status):
+            serverSocket.sendto('Register Successful!'.encode(), clientAddress)
+        else:
+            serverSocket.sendto('Username Already Registered'.encode(), clientAddress)
+                    
+    elif(command.split(' ')[0] == 'all'):
+            dataArr = command.split(' ')
+            data = ' '.join(dataArr[1:])
+            broadCast(data, clientAddress)
+            serverSocket.sendto('Message Sent!'.encode(), clientAddress)
+                    
+    elif(command.split(' ')[0] == 'pm'):
+            dataArr = command.split(' ')
+            userDestination = dataArr[1]
+            data = ' '.join(dataArr[2:])
+            privateMessage(data, userDestination, clientAddress)
+            serverSocket.sendto('Message Sent!'.encode(), clientAddress)
+                    
+    elif(command.split(' ')[0] == 'file'):
+            serverSocket.sendto('File received!'.encode(), clientAddress)
+            dataArr = command.split(' ')
+            userDestination = dataArr[1]
+            data = ' '.join(dataArr[2:])
+            sendFilePrivate(clientAddress, userDestination, data)
     else:
-        serverSocket.sendto(decodedMEssage.upper().encode(), clientAddress)
+        serverSocket.sendto('Comando não reconhecido'.encode(), clientAddress)
+                
+#Thread para ouvir os clientes        
+def listen():
+    print("Server is Listening...")
+    while (True):
+
+        try:
+            command, clientAddress = serverSocket.recvfrom(2048)
+            print(F"Listening Address: {clientAddress}")
+            client(command, clientAddress)
+
+        except timeout:      
+            continue
+        
+        except  (e):
+            print(e)
+
+
+def run () :
+    print("Starting Server...")
+    threading.Thread(target=listen()).start()
+
+
+run()
